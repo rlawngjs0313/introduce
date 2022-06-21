3ea9d8be265078da42b546ffae3c691590e6cff8
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-06-21 14:34:25 +0900

    .

--- HP/hp/src/App.js
+++ HP/hp/src/App.js
@@ -1,25 +1,53 @@
+/* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [a, astate] = useState(['남자코트 추천', '강남 맛집추천', '파이썬독학']);
+  let [good, goodstate] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nav'>
+        <h4>ReactBlog</h4>
+      </div>
+      <div className='list'>
+        <h4>{a[0]} <span onClick={()=>{goodstate(good+1)}}>👍</span> {good}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{a[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{a[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <Modal></Modal>
+      <div>
+        <span onClick={()=>{
+          let copied = [...a];
+          copied[0] = '여자코트 추천';
+          astate(copied);
+          }}> 버튼</span>
+      </div>
+      <button onClick={()=>{
+        let list = [...a];
+        list = list.sort();
+        astate(list);
+      }}>
+        가나다정렬
+      </button>
     </div>
   );
+}
+function Modal(a){
+  return (
+    <div className='list'>
+      <h4>{a[0]}</h4>
+      <p>내용</p>
+    </div>
+  )
 }
 
 export default App;
--- help.js
+++
@@ -1,6 +0,0 @@
-const names = "Mike";
-const age = 30;
-
-const message3 = `my name is ${names}`;
-console.log(message3);
-const name = prompt("이름 입력");
